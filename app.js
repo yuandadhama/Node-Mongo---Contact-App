e38f90c61f59137a3fec9b5141d596f3bf234760
yuandadhama <yuandadhamap@gmail.com>
2023-10-07 23:06:26 +0700

    fitur hapus sudah selesai dengan edit juga sudah selesai sementara. belum dilakukan pengesahan selesai

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const Contact = require('./model/contact');
 const app = express();
 const port = 3000;
 
+const { body, validationResult, check } = require('express-validator')
+
+// setup method override
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
@@ -20,6 +27,7 @@ app.use(session({
    saveUninitialized: true,
 }));
 
+// setup ejs
 app.use(flash());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -27,7 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
    console.log(`Mongo Contact App | listening at http://localhost:${port}`);
-})
+});
 
 app.get('/', (req, res) => {
    const mahasiswa = [
@@ -69,6 +77,99 @@ app.get('/contact', async (req, res) => {
       contacts,
       msg: req.flash('msg'),
    });
+});
+
+// tambah contact
+app.get('/contact/add', (req, res) => {
+   res.render('addcontact');
+});
+
+// proses tambah data contact
+app.post('/contact', [
+   body('nama').custom(async value => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (duplikat) {
+         throw new Error('nama sudah ada dalam daftar contacts');
+      }
+      return true;
+   }),
+   check('email', 'email tidak valid').isEmail(),
+   check('nohp', 'noHP tidak valid').isMobilePhone('id-ID')
+], (req, res) => {
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      res.render('addcontact', { errors: errors.array() });
+   } else {
+      Contact.insertMany(req.body).then((result) => {
+         req.flash('msg', 'data contact berhasil ditambahkan');
+         res.redirect('/contact');
+      });
+   }
+});
+
+app.delete('/contact', (req, res) => {
+   Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+      req.flash('msg', 'data contact berhasil dihapus');
+      res.redirect('/contact');
+   });
+})
+
+// app.get('/contact/delete/:nama', async (req, res) => {
+//    const contact = await Contact.findOne({ nama: req.params.nama });
+
+//    if (!contact) {
+//       res.status(400);
+//       res.send('404');
+//    } else {
+//       Contact.deleteOne({ name: req.params.name }).then((result) => {
+//          req.flash('msg', 'data contact berhasil dihapus');
+//          res.redirect('/contact');
+//       });
+//    }
+// });
+
+// edit contact
+app.get('/contact/edit/:nama', async (req, res) => {
+   const contact = await Contact.findOne({ nama: req.params.nama });
+
+   res.render('editcontact', { contact });
+});
+
+// proses edit contact
+app.put('/contact', [
+   body('nama').custom(async (value, { req }) => {
+      const duplikat = await Contact.findOne({ nama: value });
+
+      // jika value tidak sama yang lama dan duplikat
+      if (value != req.body.oldName && duplikat) {
+         throw new Error('nama sudah ada dalam daftar contacts');
+      }
+      return true;
+   }),
+   check('email', 'email tidak valid').isEmail(),
+   check('noHP', 'noHP tidak valid').isMobilePhone('id-ID')
+], (req, res) => {
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      res.render('editcontact', {
+         errors: errors.array(),
+         contact: req.body
+      });
+   } else {
+      Contact.UpdateOne(
+         { _id: req.body._id },
+         {
+            $set: {
+               nama: req.body.nama,
+               email: req.body.email,
+               nohp: req.body.nohp
+            }
+         }
+      ).then(res => {
+         req.flash('msg', 'data contact berhasil dihapus');
+         res.redirect('/contact');
+      })
+   }
 });
 
 // get data detail contact
